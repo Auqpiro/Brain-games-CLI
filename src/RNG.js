11452057f8278eb4b5844087107c0e992bab3474
Ivan Konovalov <zzoomzoone@gmail.com>
2023-01-27 21:09:03 +0400

    fix: Patch RNG

--- src/RNG.js
+++ src/RNG.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-export default function generateRandomNumber(startRange = 0, endRange = 100) {
+export default function generateRandomNumber(startRange = 1, endRange = 100) {
   const min = Math.ceil(startRange);
   const max = Math.floor(endRange);
   return Math.floor(Math.random() * (max - min + 1)) + min;
